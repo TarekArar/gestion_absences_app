9df68faaf2e6d8f6d31ee0122a19e85ce4c62935
TarekArar <gt_arar@esi.dz>
2021-03-29 17:05:01 +0100

    add routing

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { AuthProvider, useAuthContext } from "./context/AuthContext";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const { user } = useAuthContext();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Home">
+    //     <Stack.Screen
+    //       name="Login"
+    //       component={LoginScreen}
+    //       headerShown={false}
+    //     />
+    //     <Stack.Screen name="Home" component={HomeScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <AuthProvider>
+      <Stack.Navigator>
+        {user == null ? (
+          // No token found, user isn't signed in
+          // <Stack.Screen
+          //   name="SignIn"
+          //   component={SignInScreen}
+          //   options={{
+          //     title: "Sign in",
+          //     // When logging out, a pop animation feels intuitive
+          //     // You can remove this if you want the default 'push' animation
+          //     animationTypeForReplace: state.isSignout ? "pop" : "push",
+          //   }}
+          // />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            headerShown={false}
+          />
+        ) : (
+          // User is signed in
+          <Stack.Screen name="Home" component={HomeScreen} />
+        )}
+      </Stack.Navigator>
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
